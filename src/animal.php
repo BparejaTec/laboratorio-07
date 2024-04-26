65f215082fa498032c6dcbb7e10f478cff2fc7ea
bpareja-tecsup <167910880+bpareja-tecsup@users.noreply.github.com>
2024-04-26 15:52:59 -0500

    cree archivo animal.php

---
+++ src/animal.php
@@ -0,0 +1,42 @@
+class Animal {
+    // Atributos
+    private $nombre;
+    private $edad;
+    private $especie;
+
+    // Constructor
+    public function __construct($nombre, $edad, $especie) {
+        $this->nombre = $nombre;
+        $this->edad = $edad;
+        $this->especie = $especie;
+    }
+
+    // Métodos
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+
+    public function getEdad() {
+        return $this->edad;
+    }
+
+    public function setEdad($edad) {
+        $this->edad = $edad;
+    }
+
+    public function getEspecie() {
+        return $this->especie;
+    }
+
+    public function setEspecie($especie) {
+        $this->especie = $especie;
+    }
+
+    public function hacerSonido() {
+        echo "El animal hace un sonido.";
+    }
+}
